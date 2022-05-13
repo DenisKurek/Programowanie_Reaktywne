ae4deda06cf60010ed1b6d6f585bd35169d9bce4
deniskurek <denis4kurek@gmail.com>
2022-05-13 11:37:02 +0200

    lab_10

---
+++ src/components/dicaprio.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Dicaprio = () => {
+   return <div className="jumbotron jumbotron-fluid">
+       <div className="container">
+            <img src="https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/maxresdefault-1605111824.jpg"/>
+       </div>
+    </div>;
+};
+
+export default Dicaprio;
--- src/index.js
+++ src/index.js
@@ -2,13 +2,42 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'font-awesome/css/font-awesome.min.css';
 import reportWebVitals from './reportWebVitals';
+import Home from "./components/home"
+import Posts from "./components/posts"
+import NotFound from "./components/notFound"
+import Dicaprio from "./components/dicaprio"
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+      <BrowserRouter>
+          <Routes>
+              <Route path="/" element={<App/>}>
+                   
+                <Route path="home" element={<Home/>}/>
+
+                <Route path="posts" element={<Posts/>}/>
+                
+                <Route path="easter_egg" element={<Dicaprio/>}/>
+                <Route
+                       path="*"
+                       element={
+                           <NotFound/>
+                       }
+                   />
+              </Route>
+          </Routes>
+      </BrowserRouter>
   </React.StrictMode>,
-  document.getElementById('root')
+   document.getElementById('root')
+
 );
 
 // If you want to start measuring performance in your app, pass a function
